20142cfdaead67cb70d92da3dc39da801b064b3b
Valmoe <gumovalentine@gmail.com>
2024-02-10 16:44:14 +0300

    Addition with functions defined using two variables

---
+++ input.py
@@ -0,0 +1,7 @@
+number_1 = input("Enter first number \n")
+number_2 = input("Enter second number \n")
+
+def sum():
+    number_3 = int(number_1) + int(number_2)
+    print(f"The sum of {number_1} and {number_2} is {number_3}")
+sum()
