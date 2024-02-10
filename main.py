32acbf6d1aa9f401913d0f79a1df08c3304684c7
Valmoe <gumovalentine@gmail.com>
2024-02-10 16:44:14 +0300

    The first usage of functions with defined function values

---
+++ main.py
@@ -0,0 +1,11 @@
+rent = 600
+shopping = 15000
+savings = 5000
+
+def monthly_expense(months):
+    print(f"In {months} months, Valentine Gumo spends {rent*months} on rent, {shopping*months} on shopping and lastly {savings*months} retains as savings")
+
+monthly_expense(1)
+monthly_expense(12)
+monthly_expense(5690)
+
